abcba6e6d96e8327bd72d35add4c03a136c1133a
tsfoverlord <rat4521@gmail.com>
2024-04-07 15:11:17 +0530

    Implement /students endpoint to create new student

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, status, HTTPException
+from schemas import Student
+from database import students_collection
 app = FastAPI()
+
+dummy_db = []
+
+@app.post('/students', status_code=status.HTTP_201_CREATED)
+async def create_students(student: Student):
+    result = students_collection.insert_one(student.model_dump())
+    return {'id': str(result.inserted_id)}
 
 @app.get('/students')
 async def list_students():
     pass
 
-@app.post('/students')
-async def create_students():
-    pass
-    
+
 @app.get('/students/{id}')
 async def fetch_student():
     pass
