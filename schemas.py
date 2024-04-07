18ed7d7fa1b191f1dca6ef152fbb7b3db8ef29c8
tsfoverlord <rat4521@gmail.com>
2024-04-07 15:03:51 +0530

    Create required schemas

---
+++ schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Address(BaseModel):
+    city:str
+    country:str
+
+class StudentOut(BaseModel):
+    name:str
+    age:int
+
+class Student(StudentOut):
+    address:Address
+
+class AddressUpdate(BaseModel):
+    city:str|None = None
+    country:str|None = None
+
+class StudentUpdate(BaseModel):
+    name:str | None = None
+    age:str|None = None
+    address: AddressUpdate|None = None
+
