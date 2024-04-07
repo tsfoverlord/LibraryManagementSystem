2074668155ab7adf26decf25c0412a1df28facfe
tsfoverlord <rat4521@gmail.com>
2024-04-07 19:43:02 +0530

    Implement /students endpoint to return list of all students

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, status, HTTPException
-from schemas import Student
+from schemas import Student,StudentOut
 from database import students_collection
 app = FastAPI()
 
@@ -10,9 +10,24 @@ async def create_students(student: Student):
     result = students_collection.insert_one(student.model_dump())
     return {'id': str(result.inserted_id)}
 
-@app.get('/students')
-async def list_students():
-    pass
+@app.get('/students',status_code=status.HTTP_200_OK)
+async def list_students(country:str|None = None, age:int|None = None):
+    project = {
+        '_id': 0, 
+        'name': 1, 
+        'age': 1
+    }
+
+    filter = {}
+    if country:
+        filter.update({'address.country':{'$eq':country}})
+    if age:
+        filter.update({'age':{'$gte':age}})
+
+    student_list = list(students_collection.find(filter=filter, projection=project))
+
+    return {'data': student_list}
+
 
 
 @app.get('/students/{id}')
