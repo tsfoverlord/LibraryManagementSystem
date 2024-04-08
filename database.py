774275a546906e5999fb8e21bfb14033bb9e380b
tsfoverlord <rat4521@gmail.com>
2024-04-08 23:46:51 +0530

    Add exception handling

--- database.py
+++ database.py
@@ -3,11 +3,18 @@ import os
 from dotenv import load_dotenv
 from schemas import Student, StudentUpdate
 from bson import ObjectId
+from bson.errors import InvalidId
+
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 client = MongoClient(DATABASE_URL)
 lms_db = client.lib_management_system
 students_collection = lms_db.students
+
+class NotFound(Exception):
+    pass
+
+
 
 def add_student(student: Student)-> str:
     result = students_collection.insert_one(student.model_dump())
@@ -29,10 +36,13 @@ def list_students(country:str|None = None, age:int|None = None)-> list:
     return student_list
 
 def get_student_by_id(id:str):
-    result = students_collection.find(filter = {'_id':ObjectId(id)}, projection={'_id':0})
-    student = result.next()
-    result.close()
-    return student
+    try:
+        result = students_collection.find_one(filter = {'_id':ObjectId(id)}, projection={'_id':0})
+    except InvalidId:
+        raise
+    if not result:
+        raise NotFound
+    return result
 
 def update_student_by_id(id:str, fields_to_update:StudentUpdate)->None:
     student_dict = fields_to_update.model_dump()
@@ -50,8 +60,16 @@ def update_student_by_id(id:str, fields_to_update:StudentUpdate)->None:
             update_query['address.city'] = address_dict['city']
         if address_dict.get('country'):
             update_query['address.country'] = address_dict['country']
-
-    result = students_collection.find_one_and_update({'_id':ObjectId(id)}, {'$set': update_query})
-
+    try:
+        result = students_collection.find_one_and_update({'_id':ObjectId(id)}, {'$set': update_query})
+    except InvalidId:
+        raise
+    if not result:
+        raise NotFound
 def delete_student_by_id(id:str)->None:
-    result = students_collection.delete_one({'_id': ObjectId(id)})
+    try:
+        result = students_collection.delete_one({'_id': ObjectId(id)})
+    except InvalidId:
+        raise
+    if result.deleted_count == 0:
+        raise NotFound
