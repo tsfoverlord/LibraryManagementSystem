4c068172bcafbb1d63db501524fe6ebbe93bc44e
tsfoverlord <rat4521@gmail.com>
2024-04-08 23:48:40 +0530

    Handle and raise HTTP exceptions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, status, HTTPException
 from schemas import Student,StudentUpdate
 import database as db
-
+from bson.errors import InvalidId
 
 app = FastAPI()
 
@@ -17,16 +17,31 @@ async def list_students(country:str|None = None, age:int|None = None):
 
 @app.get('/students/{id}')
 async def fetch_student(id:str):
-    student = db.get_student_by_id(id)
+    try:
+        student = db.get_student_by_id(id)
+    except InvalidId:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="invalid id")
+    except db.NotFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"student with id {id} does not exist")
     return student
 
 @app.patch('/students/{id}',status_code=status.HTTP_204_NO_CONTENT)
 async def update_student(id:str, student:StudentUpdate):
-    db.update_student_by_id(id, student)
+    try:
+        db.update_student_by_id(id, student)
+    except InvalidId:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="invalid id")
+    except db.NotFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"student with id {id} does not exist")
     return
 
 @app.delete('/students/{id}', status_code=status.HTTP_200_OK)
 def delete_student(id:str):
-    db.delete_student_by_id(id)
+    try:
+        db.delete_student_by_id(id)
+    except InvalidId:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="invalid id")
+    except db.NotFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"student with id {id} does not exist")
     return {}
     
