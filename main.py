a83dd4ffaead77d32bb721bae1eb90d39af90437
tsfoverlord <rat4521@gmail.com>
2024-04-24 01:37:44 +0530

    Add rate-limit

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from schemas import Student,StudentUpdate
 import database as db
 from bson.errors import InvalidId
+import redis 
+import datetime
 
 app = FastAPI(title='Task',
               summary='A CRUD app',
@@ -16,6 +19,35 @@ app.add_middleware(CORSMiddleware,
 @app.on_event('shutdown')
 def shutdown():
     db.close_connection()
+
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+limit = 100 #100 requests per day from single student
+ttl = datetime.timedelta(days=1) #time to live for request count of each student
+
+@app.middleware('http')
+async def rate_limit(request:Request, call_next):
+    student_id = request.headers.get('X-id') #read custom header X-id
+    if not student_id:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,content={"detail":"header field 'X-id' not found"})
+    
+    #check if student exists
+    try:
+        db.get_student_by_id(student_id)
+    except InvalidId:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"detail":"invalid student id"})
+    except db.NotFound:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,content={"detail":f"student with id {student_id} does not exist"})
+
+    if r.exists(student_id):
+        r.incr(student_id) #increment request count
+        if int(r.get(student_id)) > limit:
+            return JSONResponse(status_code=status.HTTP_429_TOO_MANY_REQUESTS,content={"detail":f"{limit} requests allowed per day"})
+    else:
+        r.setex(student_id,time=ttl,value=1) #request count expires after 1 day
+
+    response = await call_next(request)
+    return response
 
 @app.post('/students', status_code=status.HTTP_201_CREATED, tags=['Students'])
 async def create_students(student: Student):
